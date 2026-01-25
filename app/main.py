a585b9a9cac956ad440cfc1446c9565d471b7ef0
zerw1337 <ivanovgleb2011@gmail.com>
2026-01-25 16:53:23 +0300

    backend stable

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import os
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)

53e8a1a74873343a8066e8e2e7296c68b907e5a6
zerw1337 <ivanovgleb2011@gmail.com>
2026-01-25 19:46:34 +0300

    front updates

--- app/main.py
+++ app/main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+@app.get("/", response_class=HTMLResponse, summary="index.html")
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})

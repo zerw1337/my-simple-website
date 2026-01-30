0e7a02c2789c93e08c8af233f5a257919c913b46
zerw1337 <ivanovgleb2011@gmail.com>
2026-01-30 19:09:45 +0300

    header complete+structure-improvements

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,18 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="Templates")
 
 @app.get("/", response_class=HTMLResponse, summary="index.html")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
+

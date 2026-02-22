1236d45e1a3664c634f6212306a6d28e2b555daf
zerw1337 <ivanovgleb2011@gmail.com>
2026-02-22 19:19:02 +0300

    dev 1st commit

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends
 from contextlib import asynccontextmanager
 import uvicorn
 
-
 from src.models.models import *
 from src.models.database import db
 from api.registration.views import register_router
@@ -10,16 +9,13 @@ from api.auth.views import auth_router
 from api.auth.schemas import UserOut
 from api.auth.dependencies import get_auth
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with db.async_engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all)
     yield
 
-
 app = FastAPI(lifespan=lifespan)
-
 
 app.include_router(register_router)
 app.include_router(auth_router)

411eaec47cb6d6a17aa7ef155b21bb32105c8150
zerw1337 <ivanovgleb2011@gmail.com>
2026-03-04 20:05:21 +0300

    dev init x3

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app.add_middleware(CORSMiddleware,
                     allow_origins=["http://127.0.0.1:5500"],
                     allow_credentials=True,
                     allow_methods=["*"],
-                    allow_headers=["*"]
+                    allow_headers=["*"],
                    )
 
 @app.get('/')
